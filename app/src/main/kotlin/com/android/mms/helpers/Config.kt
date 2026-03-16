3cb3e59456a0912f448c21a4ac248b3b42db1354
LordOfCodes26 <dtao.bittensor@gmail.com>
2026-03-16 12:12:44 -0400

    update: font size big in default

--- app/src/main/kotlin/com/android/mms/helpers/Config.kt
+++ app/src/main/kotlin/com/android/mms/helpers/Config.kt
@@ -304,7 +304,7 @@ class Config(context: Context) : BaseConfig(context) {
         set(copyNumberAndDelete) = prefs.edit { putBoolean(COPY_NUMBER_AND_DELETE_PREF, copyNumberAndDelete) }
 
     var fontSizeMessage: Int
-        get() = prefs.getInt(FONT_SIZE_MESSAGE, 0)
+        get() = prefs.getInt(FONT_SIZE_MESSAGE, 2)
         set(fontSizeMessage) = prefs.edit { putInt(FONT_SIZE_MESSAGE, fontSizeMessage) }
 
     var fontSizeMessageMultiplier: Float
