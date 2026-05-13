d65b82010212a043dc8aac55307d7e0c7095383e
pin102 <pin102@mail.com>
2026-05-13 12:53:17 -0700

    fixed: sim icon position in contact picker list

--- app/src/main/kotlin/com/android/mms/helpers/SimIconColor.kt
+++ app/src/main/kotlin/com/android/mms/helpers/SimIconColor.kt
@@ -1,6 +1,9 @@
 package com.android.mms.helpers
 
+import android.Manifest
 import android.content.Context
+import android.telephony.SubscriptionManager
+import androidx.annotation.RequiresPermission
 import com.android.mms.extensions.subscriptionManagerCompat
 import com.goodwy.commons.extensions.baseConfig
 import com.goodwy.commons.helpers.BaseConfig
@@ -31,4 +34,14 @@ fun Context.resolveSimIconTint(
 
     }
     return baseConfig.getSimIconTintForSlot(textColor, simSlotId)
+}
+
+@RequiresPermission(Manifest.permission.READ_PHONE_STATE)
+fun Context.subscriptionIdForOneBasedSimSlot(simSlot: Int): Int {
+    if (simSlot !in 1..4) return SubscriptionManager.INVALID_SUBSCRIPTION_ID
+    val subs = subscriptionManagerCompat().activeSubscriptionInfoList?: return SubscriptionManager.INVALID_SUBSCRIPTION_ID
+    subs.firstOrNull{ it.simSlotIndex == simSlot - 1}?.subscriptionId?.let { return it }
+    subs.firstOrNull{ it.simSlotIndex == simSlot }?.subscriptionId?.let { return it }
+
+    return subs.getOrNull(simSlot - 1)?.subscriptionId ?: SubscriptionManager.INVALID_SUBSCRIPTION_ID
 }
