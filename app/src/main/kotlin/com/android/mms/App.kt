d0b0bb09b296ea5b99e4f4a8ea5efc9dbc035524
pin102 <pin102@mail.com>
2026-05-19 01:48:15 -0700

    reverted: App.kt

--- app/src/main/kotlin/com/android/mms/App.kt
+++ app/src/main/kotlin/com/android/mms/App.kt
@@ -9,41 +9,24 @@ import com.goodwy.commons.RightApp
 import com.goodwy.commons.helpers.PERMISSION_READ_CONTACTS
 import com.goodwy.commons.extensions.hasPermission
 import com.android.mms.helpers.MessagingCache
-import com.android.mms.helpers.refreshConversations
-import com.android.mms.models.Events
-import org.greenrobot.eventbus.EventBus
 
-
-private const val CONTACTS_CHANGED_CONVERSAIONS_DEBOUNCE_MS = 400L
 class App : RightApp() {
     override val isAppLockFeatureAvailable = true
 
-    private val contactsChangeHandler = Handler(Looper.getMainLooper())
-    private val postContactsCacheEvictRefreshConversations = Runnable {
-//        MessagingCache.namePhoto.evictAll()
-//        MessagingCache.participantsCache.evictAll()
-        refreshConversations()
-        EventBus.getDefault().post(Events.ContactsProviderChanged())
-    }
-
     override fun onCreate() {
         super.onCreate()
-        registerGlobalContactsObservers()
-    }
 
-    private fun registerGlobalContactsObservers() {
-        val uris = listOf(
-            ContactsContract.AUTHORITY_URI,
-            ContactsContract.Contacts.CONTENT_URI,
-            ContactsContract.Data.CONTENT_URI,
-            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-            ContactsContract.DisplayPhoto.CONTENT_URI,
-        )
-
-        uris.forEach { uri: Uri ->
-            try {
-                contentResolver.registerContentObserver(uri, true, contactsObserver)
-            } catch (_: Exception) {}
+        if (hasPermission(PERMISSION_READ_CONTACTS)) {
+            listOf(
+                ContactsContract.Contacts.CONTENT_URI,
+                ContactsContract.Data.CONTENT_URI,
+                ContactsContract.DisplayPhoto.CONTENT_URI
+            ).forEach {
+                try {
+                    contentResolver.registerContentObserver(it, true, contactsObserver)
+                } catch (_: Exception){
+                }
+            }
         }
     }
 
@@ -51,11 +34,6 @@ class App : RightApp() {
         override fun onChange(selfChange: Boolean, uri: Uri?) {
             MessagingCache.namePhoto.evictAll()
             MessagingCache.participantsCache.evictAll()
-            contactsChangeHandler.removeCallbacks(postContactsCacheEvictRefreshConversations)
-            contactsChangeHandler.postDelayed(
-                postContactsCacheEvictRefreshConversations,
-                CONTACTS_CHANGED_CONVERSAIONS_DEBOUNCE_MS
-            )
         }
     }
 }
